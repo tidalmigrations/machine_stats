72b859ce4c33169ed17827a016d65f5b7bcb0c82
David Colebatch <david@subdata.com>
2017-12-13 10:57:07 -0500

    refactor objects to align with TidalMigrations.com model

--- windows/server_stats.ps1
+++ windows/server_stats.ps1
@@ -19,23 +19,28 @@ $ServerStats = {
     $Total_UsedDriveSpaceGB = $Total_DriveSpaceGB - $Total_FreeSpaceGB
 
     # Create an object to return, convert this to JSON or CSV as you need:
-    $infoObject = New-Object PSObject 
+    $server_info = New-Object PSObject 
+    $custom_fields = New-Object PSObject 
 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "ServerName" -value $CPUInfo.SystemName 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_Name" -value $CPUInfo.Name 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_Description" -value $CPUInfo.Description 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_Manufacturer" -value $CPUInfo.Manufacturer 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_NumberOfCores" -value $CPUInfo.NumberOfCores 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_L2CacheSize" -value $CPUInfo.L2CacheSize 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_L3CacheSize" -value $CPUInfo.L3CacheSize 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_SocketDesignation" -value $CPUInfo.SocketDesignation 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "OS_Name" -value $OSInfo.Caption 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "OS_Version" -value $OSInfo.Version 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "TotalPhysical_Memory_GB" -value $PhysicalMemory 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "TotalVirtual_Memory_GB" -value $OSTotalVirtualMemory 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "TotalVisable_Memory_GB" -value $OSTotalVisibleMemory
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "TotalDiskSpace_Free_GB" -value $Total_FreeSpaceGB 
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "TotalDiskSpace_Allocated_GB" -value $Total_DriveSpaceGB 
-    $infoObject
+    Add-Member -inputObject $server_info -memberType NoteProperty -name "host_name" -value $CPUInfo.SystemName 
+    Add-Member -inputObject $server_info -memberType NoteProperty -name "ram_allocated_gb" -value $OSTotalVisibleMemory
+    Add-Member -inputObject $server_info -memberType NoteProperty -name "storage_allocated_gb" -value $Total_DriveSpaceGB 
+    Add-Member -inputObject $server_info -memberType NoteProperty -name "storage_used_gb" -value $Total_UsedDriveSpaceGB 
+    Add-Member -inputObject $server_info -memberType NoteProperty -name "cpu_count" -value $CPUInfo.NumberOfCores 
+    Add-Member -inputObject $server_info -memberType NoteProperty -name "operating_system" -value $OSInfo.Caption 
+    Add-Member -inputObject $server_info -memberType NoteProperty -name "operating_system_version" -value $OSInfo.Version 
+    Add-Member -inputObject $server_info -memberType NoteProperty -name "cpu_name" -value $CPUInfo.Name 
+
+    # Custom fields:
+    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_Description" -value $CPUInfo.Description 
+    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_Manufacturer" -value $CPUInfo.Manufacturer 
+    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_L2CacheSize" -value $CPUInfo.L2CacheSize 
+    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_L3CacheSize" -value $CPUInfo.L3CacheSize 
+    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_SocketDesignation" -value $CPUInfo.SocketDesignation 
+    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "TotalPhysical_Memory_GB" -value $PhysicalMemory 
+    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "TotalVirtual_Memory_GB" -value $OSTotalVirtualMemory 
+
+    Add-Member -inputObject $server_info -memberType NoteProperty -name "custom_fields" -value $custom_fields 
+    $server_info
 }
 
