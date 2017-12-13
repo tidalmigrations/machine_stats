c792469847fa6728f21c8739d855231f38a482b1
David Colebatch <david@subdata.com>
2017-12-13 14:36:20 -0500

    handle multiple CPUs better

--- windows/server_stats.ps1
+++ windows/server_stats.ps1
@@ -2,6 +2,13 @@ $ServerStats = {
     $CPUInfo = Get-WmiObject Win32_Processor 
     $OSInfo = Get-WmiObject Win32_OperatingSystem  
 
+    if( $CPUInfo.count -gt 1 ){
+      $cpu = $CPUInfo[0]
+    } else {
+      $cpu = $CPUInfo
+    }
+    $cpu_count = $CPUInfo.count
+
     # Get Memory Information. 
     # The data will be shown in a table as MB, rounded to the nearest second decimal. 
     $OSTotalVirtualMemory = [math]::round($OSInfo.TotalVirtualMemorySize / 1MB, 2) 
@@ -22,22 +29,22 @@ $ServerStats = {
     $server_info = New-Object PSObject 
     $custom_fields = New-Object PSObject 
 
-    Add-Member -inputObject $server_info -memberType NoteProperty -name "host_name" -value $CPUInfo.SystemName 
     Add-Member -inputObject $server_info -memberType NoteProperty -name "ram_allocated_gb" -value $OSTotalVisibleMemory
+    Add-Member -inputObject $server_info -memberType NoteProperty -name "host_name" -value $cpu.SystemName 
     Add-Member -inputObject $server_info -memberType NoteProperty -name "storage_allocated_gb" -value $Total_DriveSpaceGB 
     Add-Member -inputObject $server_info -memberType NoteProperty -name "storage_used_gb" -value $Total_UsedDriveSpaceGB 
-    Add-Member -inputObject $server_info -memberType NoteProperty -name "cpu_count" -value $CPUInfo.NumberOfCores 
+    Add-Member -inputObject $server_info -memberType NoteProperty -name "cpu_count" -value ($cpu.NumberOfCores * $cpu_count)
     Add-Member -inputObject $server_info -memberType NoteProperty -name "operating_system" -value $OSInfo.Caption 
     Add-Member -inputObject $server_info -memberType NoteProperty -name "operating_system_version" -value $OSInfo.Version 
-    Add-Member -inputObject $server_info -memberType NoteProperty -name "cpu_name" -value $CPUInfo.Name 
+    Add-Member -inputObject $server_info -memberType NoteProperty -name "cpu_name" -value $cpu.Name 
 
     # Custom fields:
-    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_Description" -value $CPUInfo.Description 
-    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_Manufacturer" -value $CPUInfo.Manufacturer 
-    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_L2CacheSize" -value $CPUInfo.L2CacheSize 
-    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_L3CacheSize" -value $CPUInfo.L3CacheSize 
-    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_SocketDesignation" -value $CPUInfo.SocketDesignation 
     Add-Member -inputObject $custom_fields -memberType NoteProperty -name "TotalPhysical_Memory_GB" -value $PhysicalMemory 
+    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_Description" -value $cpu.Description 
+    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_Manufacturer" -value $cpu.Manufacturer 
+    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_L2CacheSize" -value $cpu.L2CacheSize 
+    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_L3CacheSize" -value $cpu.L3CacheSize 
+    Add-Member -inputObject $custom_fields -memberType NoteProperty -name "CPU_SocketDesignation" -value $cpu.SocketDesignation 
     Add-Member -inputObject $custom_fields -memberType NoteProperty -name "TotalVirtual_Memory_GB" -value $OSTotalVirtualMemory 
 
     Add-Member -inputObject $server_info -memberType NoteProperty -name "custom_fields" -value $custom_fields 
