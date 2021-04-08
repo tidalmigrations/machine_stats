602cfaf5a55c3de9348144b503ff5b90ed0b39e1
Petr Shevtsov <petr.shevtsov@tidalmigrations.com>
2021-04-08 16:27:01 +0300

    Initial commit

---
+++ unix/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="machine_stats",
+    version="0.0.1",
+    author="Petr Shevtsov",
+    author_email="petr.shevtsov@tidalmigrations.com",
+    description="A simple and effective way to gather machine statistics (RAM, Storage, CPU, etc.) from server environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["machine_stats"],
+    package_dir={"": "src"},
+    url="https://github.com/tidalmigrations/machine_stats",
+    install_requires=[
+        "ansible<2.10",
+    ],
+    entry_points={
+        "console_scripts": [
+            "machine_stats=machine_stats:main",
+        ]
+    },
+)
