430676cceb169de0e7a75d53f96b2a6cef9672a7
Petr Shevtsov <petr.shevtsov@tidalmigrations.com>
2021-05-04 17:10:26 +0300

    Bump version: 0.0.6 → 0.1.0

--- unix/setup.py
+++ unix/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="machine_stats",
-    version="0.0.6",
+    version="0.1.0",
     author="Petr Shevtsov",
     author_email="petr.shevtsov@tidalmigrations.com",
     description="A simple and effective way to gather machine statistics (RAM, Storage, CPU, etc.) from server environment",
