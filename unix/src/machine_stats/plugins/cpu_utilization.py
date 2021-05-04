7ea45b37b8b0b2232dc2d001e419eee2d9c54ca0
Petr Shevtsov <petr.shevtsov@tidalmigrations.com>
2021-05-04 17:00:02 +0300

    CPU utilization (#35)
    * Update requirements
    
    * Initial commit
    
    * Adding plugin manager

---
+++ unix/src/machine_stats/plugins/cpu_utilization.py
@@ -0,0 +1,27 @@
+def setup(app):
+    timeout = app.args.cpu_utilization_timeout
+    if timeout > 0:
+        app.add_playbook_tasks(
+            dict(
+                action=dict(module="cpu_utilization", args=dict(timeout=timeout)),
+            )
+        )
+
+
+def add_arguments(parser):
+    parser.add_argument(
+        "--cpu-utilization-timeout",
+        metavar="SECONDS",
+        type=int,
+        default=30,
+        help="timeout sampling for CPU utilization",
+    )
+
+
+def ok_callback(parent, result):
+    host = result._host.get_name()
+    cpu_util = result._result.get("ansible_cpu_utilization")
+    if cpu_util is not None:
+        parent.update_results(
+            host, {"cpu_average": cpu_util["average"], "cpu_peak": cpu_util["peak"]}
+        )
