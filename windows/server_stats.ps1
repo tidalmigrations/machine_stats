665c1f3a9b6b69040f4f39a7bc2ba71bc9ba4670
Petr Razumov <petr.razumov@tidalmigrations.com>
2021-09-03 15:44:05 +0300

    Do not calculate CPU utilization if timeout is 0

--- windows/server_stats.ps1
+++ windows/server_stats.ps1
@@ -1,6 +1,7 @@
 $ServerStats = {
     $CPUInfo = Get-WmiObject Win32_Processor 
     $OSInfo = Get-WmiObject Win32_OperatingSystem  
+    $CpuUtilizationTimeout = $args[0]
 
     if ($CPUInfo.count -gt 1) {
         $cpu = $CPUInfo[0]
@@ -29,18 +30,35 @@ $ServerStats = {
     }
     $Total_UsedDriveSpaceGB = $Total_DriveSpaceGB - $Total_FreeSpaceGB
 
-    $counter_params = @{
-        Counter = "\Processor(_Total)\% Processor Time"
-        SampleInterval = 1
-        MaxSamples = 30
+    $custom_fields = New-Object -TypeName psobject -Property @{
+        CPU_Description = $cpu.Description 
+        CPU_Manufacturer = $cpu.Manufacturer 
+        CPU_L2CacheSize = $cpu.L2CacheSize 
+        CPU_L3CacheSize = $cpu.L3CacheSize 
+        CPU_SocketDesignation = $cpu.SocketDesignation 
+        TotalVisible_Memory_GB = $OSTotalVisibleMemory
+        TotalVirtual_Memory_GB = $OSTotalVirtualMemory 
+        cpu_sampling_timeout = $CPUUtilization.Count
     }
-    $CPUUtilization = (Get-Counter @counter_params |
-        Select-Object -ExpandProperty countersamples |
-            Select-Object -ExpandProperty CookedValue |
-                Measure-Object -Average -Maximum)
+
+    if ($CpuUtilizationTimeout -gt 0) {
+        $counter_params = @{
+            Counter = "\Processor(_Total)\% Processor Time"
+            SampleInterval = 1
+            MaxSamples = $CpuUtilizationTimeout
+        }
+        $CpuUtilization = (Get-Counter @counter_params |
+            Select-Object -ExpandProperty countersamples |
+                Select-Object -ExpandProperty CookedValue |
+                    Measure-Object -Average -Maximum)
+        
+        Add-Member -InputObject $custom_fields -MemberType NoteProperty -Name "cpu_average" -Value $CpuUtilization.Average
+        Add-Member -InputObject $custom_fields -MemberType NoteProperty -Name "cpu_peak" -Value $CpuUtilization.Maximum
+        Add-Member -InputObject $custom_fields -MemberType NoteProperty -Name "cpu_sampling_timeout" -Value $CpuUtilization.Count
+    }
 
     # Create an object to return, convert this to JSON or CSV as you need:
-    $server_info = New-Object -TypeName psobject -Property @{
+    New-Object -TypeName psobject -Property @{
         host_name = $cpu.SystemName
         ram_allocated_gb = $PhysicalMemory 
         ram_used_gb = $OSUsedMemory 
@@ -50,20 +68,6 @@ $ServerStats = {
         operating_system = $OSInfo.Caption 
         operating_system_version = $OSInfo.Version 
         cpu_name = $cpu.Name 
+        custom_fields = $custom_fields
     } 
-    $custom_fields = New-Object -TypeName psobject -Property @{
-        CPU_Description = $cpu.Description 
-        CPU_Manufacturer = $cpu.Manufacturer 
-        CPU_L2CacheSize = $cpu.L2CacheSize 
-        CPU_L3CacheSize = $cpu.L3CacheSize 
-        CPU_SocketDesignation = $cpu.SocketDesignation 
-        TotalVisible_Memory_GB = $OSTotalVisibleMemory
-        TotalVirtual_Memory_GB = $OSTotalVirtualMemory 
-        cpu_average = $CPUUtilization.Average
-        cpu_peak = $CPUUtilization.Maximum
-        cpu_sampling_timeout = $CPUUtilization.Count
-    }
-
-    Add-Member -InputObject $server_info -MemberType NoteProperty -name "custom_fields" -value $custom_fields 
-    $server_info
 }
