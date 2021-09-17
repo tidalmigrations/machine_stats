d0d9c0f4c19503899f0b81d9d24197fbea525570
Petr Razumov <petr.razumov@tidalmigrations.com>
2021-09-17 21:40:31 +0300

    Another approach to fix CPU count

--- windows/runner.ps1
+++ windows/runner.ps1
@@ -110,10 +110,9 @@ $ServerStats = {
     } else {
         $cpu = $CPUInfo
     }
-    $cpu_count = $CPUInfo.count
-    if ($cpu_count -eq 0) {
-      $cpu_count = (Get-WmiObject Win32_ComputerSystem @getWmiObjectParams).NumberOfLogicalProcessors
-    }
+    $cpu_count = Get-WmiObject Win32_Processor @getWmiObjectParams |
+        Measure-Object -Property NumberOfCores -Sum |
+            Select-Object -ExpandProperty Sum
     # Get Memory Information. 
     # The data will be shown in a table as MB, rounded to the nearest second decimal. 
     $OSTotalVirtualMemory = [math]::round($OSInfo.TotalVirtualMemorySize / 1MB, 2) 
