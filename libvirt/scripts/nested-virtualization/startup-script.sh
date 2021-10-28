56e383bdf79208af82a4a85beae8610841e68dba
Petr Razumov <petr.razumov@tidalmigrations.com>
2021-10-28 20:03:41 +0300

    Make startup script work on RHEL/CentOS too

--- libvirt/scripts/nested-virtualization/startup-script.sh
+++ libvirt/scripts/nested-virtualization/startup-script.sh
@@ -1,17 +1,44 @@
 #!/usr/bin/env bash
 
-apt update
-apt install -y \
-  uml-utilities \
-  qemu-kvm \
-  bridge-utils \
-  virtinst \
-  libvirt-daemon-system \
-  libvirt-clients
+# shellcheck source=/dev/null
+source "/etc/os-release"
 
-virsh net-start default
+if [[ "$ID" == "rhel" || "$ID" == "centos" ]]
+then
+  # Use yum
+  yum install -y \
+    qemu-kvm \
+    libvirt \
+    libvirt-python \
+    libguestfs-tools \
+    virt-install
+fi
 
-tunctl -t tap0
+if [[ "$ID" == "debian" || "$ID" == "ubuntu" ]]
+then
+  # Use apt
+  apt update
+  apt install -y \
+    uml-utilities \
+    qemu-kvm \
+    bridge-utils \
+    virtinst \
+    libvirt-daemon-system \
+    libvirt-clients
+fi
+
+# Needs to explicitly start on RHEL 7
+systemctl start libvirtd 
+# Start the default network if not already started
+virsh net-list --name | grep --fixed-strings --line-regexp --quiet default || virsh net-start default
+
+# There is no tunctl on RHEL 7
+if type -P tunctl &>/dev/null
+then
+  tunctl -t tap0
+else
+  ip tuntap add tap0 mode tap
+fi
 ifconfig tap0 up
 
 brctl addif virbr0 tap0
