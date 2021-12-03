dafe62b3b5a9c691bb7a0b84439979734fae40bd
Petr Razumov <petr.razumov@tidalmigrations.com>
2021-12-03 19:05:40 +0300

    feat(unix): adding --cpu-utilization-only-value flag (#123)

--- unix/src/machine_stats/plugins/cpu_utilization.py
+++ unix/src/machine_stats/plugins/cpu_utilization.py
@@ -1,20 +1,32 @@
+import argparse
+
+
 def setup(app):
     timeout = app.args.cpu_utilization_timeout
+    only_value = app.args.cpu_utilization_only_value
     if timeout > 0:
         app.add_playbook_tasks(
             dict(
-                action=dict(module="cpu_utilization", args=dict(timeout=timeout)),
+                action=dict(
+                    module="cpu_utilization",
+                    args=dict(timeout=timeout, only_value=only_value),
+                ),
             )
         )
 
 
-def add_arguments(parser):
+def add_arguments(parser: argparse.ArgumentParser):
     parser.add_argument(
         "--cpu-utilization-timeout",
         metavar="SECONDS",
         type=int,
         default=30,
         help="timeout sampling for CPU utilization",
+    )
+    parser.add_argument(
+        "--cpu-utilization-only-value",
+        action="store_true",
+        help="calculate only CPU utilization value and not peak and average values",
     )
 
 
@@ -23,13 +35,24 @@ def ok_callback(parent, result):
     cpu_sampling_timeout = result._result.get("timeout")
     cpu_util = result._result.get("ansible_cpu_utilization")
     if cpu_util is not None:
-        parent.update_results(
-            host,
-            {
-                "custom_fields": {
-                    "cpu_sampling_timeout": cpu_sampling_timeout,
-                    "cpu_average": cpu_util["average"],
-                    "cpu_peak": cpu_util["peak"],
-                }
-            },
-        )
+        if "value" in cpu_util:
+            parent.update_results(
+                host,
+                {
+                    "custom_fields": {
+                        "cpu_sampling_timeout": cpu_sampling_timeout,
+                        "cpu_utilization": cpu_util["value"],
+                    }
+                },
+            )
+        else:
+            parent.update_results(
+                host,
+                {
+                    "custom_fields": {
+                        "cpu_sampling_timeout": cpu_sampling_timeout,
+                        "cpu_average": cpu_util["average"],
+                        "cpu_peak": cpu_util["peak"],
+                    }
+                },
+            )
