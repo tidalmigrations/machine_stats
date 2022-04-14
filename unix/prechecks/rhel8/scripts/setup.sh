60172cff4e9efcd892d259fa8079831a769d3408
Pranav Malaviya <pranavmalaviya2@gmail.com>
2022-04-14 19:42:24 +0530

    Get branch name

--- unix/prechecks/rhel8/scripts/setup.sh
+++ unix/prechecks/rhel8/scripts/setup.sh
@@ -8,10 +8,10 @@ echo ++ Install git
 sudo yum install -y git
 
 echo ++ Clone machine stats repo
-pwd
+echo $BRANCH_NAME
 git clone https://github.com/tidalmigrations/machine_stats.git /home/ec2-user/machine_stats
 cd /home/ec2-user/machine_stats
-git checkout $BRANCH_NAME
+git pull && git checkout $BRANCH_NAME
 
 echo ++ Install pipenv
 sudo python3 -m pip install pipenv
