39c2b946229f10889d49f0acbb046638ef11509d
Pranav Malaviya <pranavmalaviya2@gmail.com>
2022-04-21 16:38:56 +0530

    Revert back the temporary trigger

--- unix-alpha/setup.py
+++ unix-alpha/setup.py
@@ -4,7 +4,7 @@ with open("alpha-description.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="machine_stats_bojack",
+    name="machine_stats_alpha",
     version="develop",
     author="Tidal Migrations",
     author_email="support@tidalmigrations.com",
@@ -21,8 +21,8 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "machine_stats_bojack=machine_stats:main",
-            "machine-stats-bojack=machine_stats:main",
+            "machine_stats_alpha=machine_stats:main",
+            "machine-stats-alpha=machine_stats:main",
         ]
     },
 )
