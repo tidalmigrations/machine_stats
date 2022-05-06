7df1b3745524c8a13091bdd5c1ebf227b29bb966
Pranav Malaviya <pranavmalaviya2@gmail.com>
2022-05-06 08:05:59 +0530

    Add a basic ansible runner functionality with the playbook

---
+++ unix-alpha/src/machine_stats/modules/raw/raw_application.py
@@ -0,0 +1,7 @@
+import ansible_runner
+
+def run():
+    r = ansible_runner.run(private_data_dir='./src/machine_stats/modules/raw',
+                       host_pattern='all',
+                       module='raw',
+                       module_args="lsb_release -si")
