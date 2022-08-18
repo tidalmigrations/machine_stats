ed7562778a6bcda1548d017abed210abb0173dc6
Sam King <samuel-king@live.com.au>
2022-08-18 16:21:21 +0530

    Added -Measurements flag which specifies the output data structure

--- windows/runner.ps1
+++ windows/runner.ps1
@@ -34,14 +34,19 @@ The default value is 30.
 
 Capture point-in-time CPU utilization value rather than a peak and average over a given time.
 
+.PARAMETER Measurements
+
+Specifies if the data should be output in a structure that matches the TMP /measurements endpoint.
+
 .INPUTS
 
 None. You cannot pipe objects to runner.ps1
 
 .OUTPUTS
 
-runner.ps1 generates a JSON output with all the gathered data suitable to be
-used with Tidal Tools or Tidal Migrations API.
+Default behaviour will output JSON which is suitable to pipe to the Tidal Tools command "tidal sync servers".
+
+Adding the -Measurements flag will output JSON which is suitable to pipe to the Tidal Migrations Platform /measurements API endpoint.
 
 .EXAMPLE
 
@@ -84,8 +89,18 @@ param (
 
     [Parameter()]
     [switch]
-    $CpuUtilizationOnlyValue
+    $CpuUtilizationOnlyValue,
+
+    [Parameter()]
+    [switch]
+    $Measurements
 )
+
+# If the -Measurements flag is used, set -CPUUtilizationOnlyValue and -CPUUtilizationTimeout 1
+if ($Measurements) {
+    $CpuUtilizationOnlyValue = $true
+    $CpuUtilizationTimeout = 1
+}
 
 if (![System.IO.File]::Exists($SecurePwdFilePath)) {
     Write-Error "$SecurePwdFilePath does not exist. Be sure to run save_password.ps1 before trying again."
@@ -122,7 +137,7 @@ $server_list | ForEach-Object {
     if ($NoWinRM) {
         $startJobParams = @{
             ScriptBlock  = $ServerStats
-            ArgumentList = $_, $cred, $ProcessStats, $CpuUtilizationTimeout, $CpuUtilizationOnlyValue, $NoWinRM
+            ArgumentList = $_, $cred, $ProcessStats, $CpuUtilizationTimeout, $CpuUtilizationOnlyValue, $NoWinRM, $Measurements
         }
         $jobs += Start-Job @startJobParams
     } else {
@@ -130,7 +145,7 @@ $server_list | ForEach-Object {
             ComputerName = $_
             Credential   = $cred
             ScriptBlock  = $ServerStats
-            ArgumentList = "localhost", $null, $ProcessStats, $CpuUtilizationTimeout, $CpuUtilizationOnlyValue, $NoWinRM
+            ArgumentList = "localhost", $null, $ProcessStats, $CpuUtilizationTimeout, $CpuUtilizationOnlyValue, $NoWinRM, $Measurements
         }
         $jobs += Invoke-Command @invokeCommandParams -AsJob
     }
@@ -161,8 +176,16 @@ $jobs | Receive-Job | ForEach-Object {
 $num_results = $server_stats.Count
 Write-Host "$num_results results received out of $num_servers servers."
 
-# Output results
-$results = @{ servers = $server_stats }
+## Build result
+
+# Set root object key
+$root_object_key = "servers"
+if ($Measurements) {
+    $root_object_key = "measurements"
+}
+
+# output result
+$results = @{ $root_object_key = $server_stats }
 $json = $results | ConvertTo-Json -Depth 99
 Write-Output $json
 
